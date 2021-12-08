116d157e6538a6e224c84493f73120f47f8b38af
estonec <rusecru@gmail.com>
2021-12-08 15:07:47 +0300

    First try

---
+++ src/com/company/Car.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Car extends Transport{
+
+    public Car(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+
+    @Override
+    public void checkTrailer() {
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
 
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
+        Transport car = new Car("car1", 4);
+        Transport car2 = new Car("car2", 4);
+
+        Transport truck = new Truck("truck1", 6);
+        Transport truck2 = new Truck("truck2", 8);
+
+        Transport bicycle = new Bicycle("bicycle1", 2);
+        Transport bicycle2 = new Bicycle("bicycle2", 2);
+
+        ServiceStation station = new ServiceStation();
+        station.check(bicycle);
+        station.check(bicycle2);
+        station.check(car);
+        station.check(car2);
+        station.check(truck);
+        station.check(truck2);
     }
 }
+
